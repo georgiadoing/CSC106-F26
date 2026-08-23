50bb34ac2a31f7fbb5c04d057b89c59aa49367f3
georgiadoing <georgia.doing@jax.org>
2026-08-23 16:34:04 -0400

    Copy from S26

---
+++ Data/sum_of_powers.py
@@ -0,0 +1,15 @@
+# Replace this comment with an appropriate header comment.
+
+# Replace this comment with your function definition.
+
+    
+
+### DO NOT DELETE THIS LINE: beg testing
+
+# Use this area if you want to add some function calls with different
+# parameter values to test your function definition.
+
+# E.g. the sum of the first five squares should be 55.
+print(sum_of_powers(2, 5))
+
+
